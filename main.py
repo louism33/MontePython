147f83545c19a85c156ccbde5d9a8e77983b3834
Louis <louis.mackenziesmith@googlemail.com>
2018-12-23 22:55:26 +0100

    working verison

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 # a basic noughts and crosses game in python
 import sys
 import util.util
-
-#board = 0b0000000000000000000
-
-
+import random
 
 turnConstant = 0b1000000000000000000
 def getTurnBit(board): # x starts
@@ -27,14 +24,14 @@ def printBoard(n):
         elif ((n & (mask << 9)) == (mask << 9)):
             sys.stdout.write("o | ")
         else:
-            sys.stdout.write(str(t-1)+" | ")
-            #sys.stdout.write(". | ")
+            #sys.stdout.write(str(t-1)+" | ")
+            sys.stdout.write(". | ")
         mask = mask * 2
         if ((t % 3 == 0) & (t < 8)):
             sys.stdout.write("\n | ")
     sys.stdout.write("\n")
     sys.stdout.write("\n")
-    if getTurnBit(board):
+    if getTurnBit(n):
         sys.stdout.write("It is o's turn")
     else:
         sys.stdout.write("It is x's turn")
@@ -75,33 +72,34 @@ def checkForEnd(board):
 
     diag = 0b100010001
     antiDiag = 0b1010100
+
+    if len(getLegalMoves(board)) == 0:
+        return 0
+
     for i in range(-1, 2, 2):
         if i == 1:
             board = board >> 9
         if rowOne & board == rowOne:
             return i
-        if rowTwo & board == rowOne:
+        if rowTwo & board == rowTwo:
             return i
-        if rowThree & board == rowOne:
-            return i
-
-        if colOne & board == rowOne:
-            return i
-        if colTwo & board == rowOne:
-            return i
-        if colThree & board == rowOne:
+        if rowThree & board == rowThree:
             return i
 
-        if diag & board == rowOne:
+        if colOne & board == colOne:
             return i
-        if antiDiag & board == rowOne:
+        if colTwo & board == colTwo:
+            return i
+        if colThree & board == colThree:
             return i
 
+        if diag & board == diag:
+            return i
+        if antiDiag & board == antiDiag:
+            return i
+    return None
 
-
-board = 0b0000000000000000000
-#board = 0b0111
-def start():
+def standalone():
     global board
     while True:
         printBoard(board)
@@ -120,12 +118,136 @@ def start():
                 board = makeLegalMoveSafeIndex(board, move)
                 result = checkForEnd(board)
                 if result == 1:
+                    printBoard(board)
                     print("win for o")
                     return 1
                 if result == -1:
+                    printBoard(board)
                     print("win for x")
                     return -1
                 break
 
+def selectRandomMove(moves):
+    return moves[random.randint(0, len(moves) - 1)]
 
-start()
+def getRandomGame(board):
+    while True:
+        result = checkForEnd(board)
+        if result != None:
+            return result
+
+        moves = getLegalMoves(board)
+        if len(moves) == 0:
+            return 0
+        move = selectRandomMove(moves)
+        board = makeLegalMoveSafeIndex(board, move)
+        result = checkForEnd(board)
+        if result == 1:
+            return 1
+        if result == -1:
+            return -1
+
+def selectRandomRootIndex(moves):
+    return random.randint(0, len(moves) - 1)
+
+class Tree:
+    def __init__(self, parent, board, index):
+        self.parent = parent
+        self.board = board
+        self.score = 0
+        self.total = 0
+        self.getMoves()
+        self.index = index
+        self.children = [0] * len(self.moves)
+        self.childrenScores = [0] * len(self.moves)
+
+    def __str__(self):
+        self.getMoves()
+        return ("total is: " + str(self.total) + ", score: "
+        + str(self.score) + ", moves: " + str(self.moves)
+        #+ "\nchildren: " + str(self.children)
+        + "\nchildren scores: " + str(self.childrenScores))
+
+    def getMoves(self):
+        self.moves = getLegalMoves(self.board)
+        return self.moves
+
+    def backProp(self, result, i):
+        self.total += 1
+        self.score += result
+        self.childrenScores[i] += result
+
+        if self.parent != None:
+            #self.parent.childrenScores[i] += a
+            self.parent.backProp(result, self.index)
+
+    def simulate(self):
+        a = getRandomGame(self.board)
+        self.total += 1
+        self.score += a
+        if self.parent != None:
+            self.parent.backProp(a, self.index)
+
+
+    def expand(self):
+        result = checkForEnd(self.board)
+        if result != None:
+            """
+            self.total += 1
+            self.score += result
+            if self.parent != None:
+                self.parent.backProp(result, self.index)
+            """
+            return self
+
+        else:
+            self.getMoves()
+            i = selectRandomRootIndex(self.moves)
+            move = self.moves[i]
+
+            if self.children[i] == 0:
+                newBoard = makeLegalMoveSafeIndex(self.board, i)
+                self.children[i] = Tree(self, newBoard, i)
+                return self.children[i]
+            else:
+                return self.children[i].expand()
+
+
+"""
+selection expansion simulation backprop
+"""
+
+def getAIMove():
+    global masterBoard
+    t = 0
+
+    rootTree = Tree(None, masterBoard, -1)
+    #print("Root tree:")
+    #printBoard(rootTree.board)
+
+    while t < 10000:
+        t += 1
+        selectedTree = rootTree.expand()
+        selectedTree.simulate()
+
+    if getTurnBit(rootTree.board):
+        bestMoveIndex = rootTree.childrenScores.index(max(rootTree.childrenScores))
+    else:
+        bestMoveIndex = rootTree.childrenScores.index(min(rootTree.childrenScores))
+
+    return rootTree.moves[bestMoveIndex]
+
+
+def childrenBreakdown(tree):
+    print()
+    print()
+    print("\n-----Children breakdown-----\n")
+    for t in tree.children:
+        if t != 0:
+            printBoard(t.board)
+            print(t)
+
+masterBoard = 0b1000000000000000000
+#board = 0b11000000000000110
+
+print(getAIMove())
